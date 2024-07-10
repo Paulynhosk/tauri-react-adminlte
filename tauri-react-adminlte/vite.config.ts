589d6f393be948f2b77a395089b4eb991d50bf1c
Paulynhosk <paulynhosk@gmail.com>
2024-07-10 02:38:22 -0300

    Update package

--- tauri-react-adminlte/vite.config.ts
+++ tauri-react-adminlte/vite.config.ts
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  clearScreen: false,
+  server: {
+    port: 5171,
+    strictPort: true,
+  },
+  envPrefix: ['VITE_', 'TAURI_PLATFORM', 'TAURI_ARCH', 'TAURI_FAMILY', 'TAURI_PLATFORM_VERSION', 'TAURI_PLATFORM_TYPE', 'TAURI_DEBUG'],
+  build: {
+    target: process.env.TAURI_PLATFORM == 'windows' ? 'chrome105' : 'safari13',
+    minify: !process.env.TAURI_DEBUG ? 'esbuild' : false,
+    sourcemap: !!process.env.TAURI_DEBUG,
+  },
 })
